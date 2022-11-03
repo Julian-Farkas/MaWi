3c9e5f96cddd91778dfcdb85a7cc8c0a7ae52dda
Julian-Farkas <julian.farkas.ger@gmail.com>
2022-11-03 14:58:23 +0100

    Aufgabe 2 gibt nun Antworten umgekehrt aus

--- uebung1/ue1a2.cpp
+++ uebung1/ue1a2.cpp
@@ -3,16 +3,26 @@
 
 int main ()
 {
-    float piAnswer = 0.0;
+    float answer1 = 0.0;
     std::cout << "Geben Sie Pi mit den ersten zwei Ziffern an:" << std::endl;
-    std::cin >> piAnswer;
+    std::cin >> answer1;
 
-    short int bitAnswer = 8;
-    std::cout << "Wie viele Bit hat der \"Char\"-Datentyp??" <<std::endl;
-    std::cin >> bitAnswer;
+    short int answer2 = 0;
+    std::cout << "Wie viele Bit hat der \"Char\"-Datentyp??" << std::endl;
+    std::cin >> answer2;
 
-    std::string nameAnswer = "";
-    std::cout << "Wie heissen Sie?";
-    std::cin >> nameAnswer;
+    std::string answer3 = "";
+    std::cout << "Wie heissen Sie?" << std::endl;
+    std::cin >> answer3;
+
+    char answer4 = 'a';
+    std::cout << "Wie lautet der 3. Buchstabe des Alphabets?" << std::endl;
+    std::cin >> answer4;
+
+    std::string answer5 = "";
+    std::cout << "Wie heißt das Betriebssysstem, was Sie gerade benutzen?" << std::endl;
+    std::cin >> answer5;
+
+    std::cout << answer5 << " " << answer4 << " " << answer3 << " " << answer2 << " " << answer1 << std::endl;
     return 0;
 }
