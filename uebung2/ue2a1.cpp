1a3b05bb40082539ce928d45ae4b7060d935db34
Julian-Farkas <julian.farkas.ger@gmail.com>
2022-11-07 10:52:58 +0100

    Kommentare mit Lösungswege hinzugefügt

--- uebung2/ue2a1.cpp
+++ uebung2/ue2a1.cpp
@@ -9,30 +9,38 @@ int main ()
 
     // 2.)
 
-    std::cout << "Wert: " << (a * b - - - - - - 12.0f) 
+    std::cout << "Wert: " << (a * b - - - - - - 12.0f)
+                    // 5 * 2 * (-1)**6 * 12.0 = 22.0 
     << "\t Typ: Float"
     << std::endl;
 
     // 3.)
 
     std::cout << "Wert: " << (a / b / c) 
+                    // 5 / 2 / 2.0 = 2.0
     << "\t Typ: Double" 
     << std::endl;
 
     // 4.)
 
     std::cout << "Wert: " << (a / c / b)
+                    // 5 / 2.0 / 2 = 1,25
     << "\t Typ: Double"
     << std::endl;
 
     // 5.)
 
     std::cout << "Wert: " << (-1.0f - a * 2E-1 + a / 2)
-    << "\t Typ: Double" // anscheinend wird 2E-1 als Double behandelt
+                    // -1,0 - 5 * 2*10**-1 + 2 = -1,0 - 1,0 + 2 = 0
+    << "\t Typ: Double" // E-1 wird als Double behandelt
     << std::endl;
 
     // 6.)
     std::cout << "Wert: " << ( 1.0 + ( a *= ( 2 / - - ++b - ( c += .0e2 ) ) ) )
+                    /* c = 2.0 + 0.0 * 10**2 = 2.0
+                    ** a *= 2 / (b + 1) - 2.0 *= 0 - 2 *= -2 = -10
+                    ** 1.0 + -10 = -9
+                    */ 
     << "\t Typ: Double" // Da c mit .0e2 addiert wird, wird es als Double behandelt
     <<std::endl;
 
