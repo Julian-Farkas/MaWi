4dcaf9af2cff7a22c7aa0706215a357dabec44a6
Julian-Farkas <julian.farkas.ger@gmail.com>
2022-11-07 10:41:09 +0100

    Ausgabe wird als Bool ausgegeben

--- uebung2/ue2a2.cpp
+++ uebung2/ue2a2.cpp
@@ -6,35 +6,35 @@ int main()
     int idrei = 3;
     double dvier = 4.0;
 
-    std::cout << "ifuenf > idrei = " << (ifuenf > idrei) << std::endl;
+    std::cout << "ifuenf > idrei = " << bool(ifuenf > idrei) << dvier <<std::endl;
                 //true
 
-    std::cout << "dvier < idrei " << (dvier < idrei) << std::endl;
+    std::cout << "dvier < idrei " << bool(dvier < idrei) << std::endl;
                 //false
 
-    std::cout << "dvier > idrei && dvier > ifuenf " << (dvier > idrei && dvier > ifuenf) << std::endl;
+    std::cout << "dvier > idrei && dvier > ifuenf " << bool(dvier > idrei && dvier > ifuenf) << std::endl;
                 //true         && false == false
 
-    std::cout << "dvier > idrei || dvier > ifuenf " << (dvier > idrei || dvier > ifuenf) << std::endl;
+    std::cout << "dvier > idrei || dvier > ifuenf " << bool(dvier > idrei || dvier > ifuenf) << std::endl;
                 //true          || false == true
 
-    std::cout << "idrei / ifuenf " << (idrei / ifuenf) << std::endl;
+    std::cout << "idrei / ifuenf " << bool(idrei / ifuenf) << std::endl;
                 // 3    / 5     = 0 == false
 
-    std::cout << "idrei / dvier " << (idrei / dvier) << std::endl;
+    std::cout << "idrei / dvier " << bool(idrei / dvier) << std::endl;
                 // 3    / 4.0   = 0.75 != 0 == true
 
-    std::cout << "idrei - ifuenf " << (idrei - ifuenf) << std::endl;
+    std::cout << "idrei - ifuenf " << bool(idrei - ifuenf) << std::endl;
                 //3     - 5     = -2 !=0 == true
 
-    std::cout << "idrei = ifuenf " << (idrei = ifuenf) << std::endl;
+    std::cout << "idrei = ifuenf " << bool(idrei = ifuenf) << std::endl;
                 //idrei = 5 : Zuweisung erfolgt : true
 
-    //    std::cout << "ifünf != 4 || dvier == 4 " << (ifünf != 4 || dvier == 4) << std::endl;
+    //    std::cout << "ifünf != 4 || dvier == 4 " << bool(ifünf != 4 || dvier == 4) << std::endl;
     //    Variable ifuenf ist falsch geschrieben : Compilerfehler
     
-    std::cout << "5.0 <= dvier <= 6.0 " << (5.0 <= dvier <= 6.0) << std::endl;
-                // false, da 5.0 > dvier
+    std::cout << "5.0 <= dvier <= 6.0 " << bool(5.0 <= dvier <= 6.0) << std::endl;
+                // true, da Ausdruck as 5.0 <= dvier || dvier <= 6.0 interpretiert wird.
 
     return 0;    
 }
