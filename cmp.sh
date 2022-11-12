9ae8c6dc741ae2604fd8401921d8442334e46d99
Julian-Farkas <julian.farkas.ger@gmail.com>
2022-11-12 15:15:41 +0100

    Bashscript, um Code leichter zu prüfen

---
+++ cmp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#function to compile and execute .cpp:
+compile_and_execute () {
+
+    #get the basename of path without extention to autoname exe:
+    exeName=$(basename $1 .cpp)
+
+    c++ $1 -o $exeName
+    echo -e "\n\e[0;32mCompilierung erfolgreich, \"$exeName\" wird nun ausgefuehrt:\e[0m\n"
+
+    #execute freshly compiled program:
+    ./$exeName
+}
+
+#check if file exists:
+if [ -e $1 ]
+then
+    #check if file has .cpp-extention:
+    if [[ "$1" == *.cpp ]]
+    then
+        compile_and_execute "$1"
+    else
+        echo -e "\n\e[7;31mFehler:\e[0;31m Die angegebene Datei \"$1\" ist keine *.cpp!\e[0m\n"
+    fi
+else
+    echo -e "\n\e[7;31mFehler:\e[0;31m Die angegebene Datei \"$1\" wurde nicht gefunden!\e[0m\n"
+fi
