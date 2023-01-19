7e6354e086b6096ddbe448ffee506c3ab3a0680d
Julian-Farkas <julian.farkas.ger@gmail.com>
2023-01-19 21:00:57 +0100

    Aufgabe 1 fertig

---
+++ uebung9/ue9a1_encode.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string encode_mail_adress (std::string mail)
+{
+    std::string encoded = "";
+
+    for (size_t i = 0; i < mail.length(); ++i)
+    {
+        switch (mail[i])
+        {
+        case '@':
+            encoded += "(at)";
+            break;
+        
+        case '.':
+            encoded += "(dot)";
+            break;
+        
+        case '-':
+            encoded += "(minus)";
+            break;
+        
+        default:
+            encoded +=  mail[i];
+            break;
+        }
+    }
+
+    return encoded;
+}
+
+int main()
+{
+    std::vector<std::string> output;
+
+    std::string input;
+
+    do
+    {
+        std::cout << "Bitte geben Sie eine der umzuwandelnden Mailadressen an (leere Eingabe startet die Umwandlung): " << std::endl;
+        std::getline(std::cin, input);
+
+        if (input != "") output.push_back( encode_mail_adress(input) );
+
+    } while (input != "");
+
+    for (std::string str : output) std::cout << str << std::endl;
+
+    return 0;
+}
