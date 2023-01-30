26f3abda59c0090d4aa78d4409eef0634194fe1d
Julian-Farkas <julian.farkas.ger@gmail.com>
2023-01-30 23:24:56 +0100

    Aufgabe 3 fertig

--- uebung11/ue11a3.cpp
+++ uebung11/ue11a3.cpp
@@ -2,89 +2,91 @@
 #include <vector>
 #include <string>
 
+
+
 //nimmt Ziffer und fügt die benötigten Zeichen dem Vektor hinzu:
 void digit_to_display (unsigned int digit, std::vector< std::vector<std::string> > &display)
 {
     switch (digit)
     {
     case 0:
-        display[0].push_back(" __  ");
-        display[1].push_back("|  | ");
-        display[2].push_back("|  | ");
-        display[3].push_back("|  | ");
-        display[4].push_back("|__| ");
+        display[0].push_back(" __ ");
+        display[1].push_back("|  |");
+        display[2].push_back("|  |");
+        display[3].push_back("|  |");
+        display[4].push_back("|__|");
         break;
     
     case 1:
-        display[0].push_back("  |  ");
-        display[1].push_back("  |  ");
-        display[2].push_back("  |  ");
-        display[3].push_back("  |  ");
-        display[4].push_back("  |  ");
+        display[0].push_back("    ");
+        display[1].push_back("  | ");
+        display[2].push_back("  | ");
+        display[3].push_back("  | ");
+        display[4].push_back("  | ");
         break;
 
     case 2:
-        display[0].push_back(" __  ");
-        display[1].push_back("   | ");
-        display[2].push_back(" __| ");
-        display[3].push_back("|    ");
-        display[4].push_back("|__  ");
+        display[0].push_back(" __ ");
+        display[1].push_back("   |");
+        display[2].push_back(" __|");
+        display[3].push_back("|   ");
+        display[4].push_back("|__ ");
         break;
 
     case 3:
-        display[0].push_back(" __  ");
-        display[1].push_back("   | ");
-        display[2].push_back(" __| ");
-        display[3].push_back("   | ");
-        display[4].push_back(" __| ");
+        display[0].push_back(" __ ");
+        display[1].push_back("   |");
+        display[2].push_back(" __|");
+        display[3].push_back("   |");
+        display[4].push_back(" __|");
         break;
 
     case 4:
-        display[0].push_back("     ");
-        display[1].push_back("|  | ");
-        display[2].push_back("|__| ");
-        display[3].push_back("   | ");
-        display[4].push_back("   | ");
+        display[0].push_back("    ");
+        display[1].push_back("|  |");
+        display[2].push_back("|__|");
+        display[3].push_back("   |");
+        display[4].push_back("   |");
         break;
 
     case 5:
-        display[0].push_back(" __  ");
-        display[1].push_back("|    ");
-        display[2].push_back("|__  ");
-        display[3].push_back("   | ");
-        display[4].push_back(" __| ");
+        display[0].push_back(" __ ");
+        display[1].push_back("|   ");
+        display[2].push_back("|__ ");
+        display[3].push_back("   |");
+        display[4].push_back(" __|");
         break;
 
     case 6:
-        display[0].push_back(" __  ");
-        display[1].push_back("|    ");
-        display[2].push_back("|__  ");
-        display[3].push_back("|  | ");
-        display[4].push_back("|__| ");
+        display[0].push_back(" __ ");
+        display[1].push_back("|   ");
+        display[2].push_back("|__ ");
+        display[3].push_back("|  |");
+        display[4].push_back("|__|");
         break;
 
     case 7:
-        display[0].push_back(" __  ");
-        display[1].push_back("   | ");
-        display[2].push_back("   | ");
-        display[3].push_back("   | ");
-        display[4].push_back("   | ");
+        display[0].push_back(" __ ");
+        display[1].push_back("   |");
+        display[2].push_back("   |");
+        display[3].push_back("   |");
+        display[4].push_back("   |");
         break;
 
     case 8:
-        display[0].push_back(" __  ");
-        display[1].push_back("|  | ");
-        display[2].push_back("|__| ");
+        display[0].push_back(" __ ");
+        display[1].push_back("|  |");
+        display[2].push_back("|__|");
         display[3].push_back("|  |");
-        display[4].push_back("|__| ");
+        display[4].push_back("|__|");
         break;
 
     case 9:
-        display[0].push_back(" __  ");
-        display[1].push_back("|  | ");
-        display[2].push_back("|__| ");
-        display[3].push_back("   | ");
-        display[4].push_back(" __| ");
+        display[0].push_back(" __ ");
+        display[1].push_back("|  |");
+        display[2].push_back("|__|");
+        display[3].push_back("   |");
+        display[4].push_back(" __|");
         break;
     }
 
@@ -120,13 +122,23 @@ void number_to_display (unsigned int number, std::vector< std::vector<std::strin
 
 void print_display (std::vector< std::vector<std::string> > &display)
 {
+    for (std::vector<std::string> row : display)
+    {
+        for (std::string col : row)
+        {
+            std::cout << col << " ";
+        }
 
+        std::cout << std::endl;
+    }
+
+    return;
 }
 
 int main()
 {
-    // Vektor zum Anzeigen erstellen:
-    std::vector< std::vector<std::string> > display;
+    // Vektor zum Anzeigen mit 5 Reihen erstellen:
+    std::vector< std::vector<std::string> > display(5);
 
     std::cout << "Zahl:\t";
     std::cout.flush();
@@ -134,8 +146,8 @@ int main()
     unsigned int number = 0;
     std::cin >> number;
 
-    //number_to_display(number, display);
-    //print_display(display);
+    number_to_display(number, display);
+    print_display(display);
 
     return 0;
 }
